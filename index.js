69fa6a36cafaa17d2140597c62b8712dbc8b0fa1
LilyClausA <lilcla1230@students.maquoketaschools.org>
2026-02-26 14:08:59 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 
+function markAsDone(todos) {
+let newTodos = [];
+  
+  todos.forEach(function(todo) {
+    newTodos.push("done -" + todos);
+  });
+  return newTodos;
+}
+
+
+//should create a function named markAsDone that 
+//takes an array of todos and returns a new array containing the modified todo items
+
+//should loop through the todos, using forEach,
+//and prepend the string 'done - ' to each todo description
